2414524e5182c767fe28d6b805a80264f9ace0f3
yonekawa <yonekawa@e5899088-dff8-4fac-bb51-c4f79c91912d>
2007-07-02 06:50:00 +0000

    ''
    git-svn-id: https://www.seasar.org/svn/sandbox/s2ana.php5/trunk@22 e5899088-dff8-4fac-bb51-c4f79c91912d

--- s2ana.php5/examples/with_s2base/app/modules/s2analogin/entity/S2AnA_LoginUser.class.php
+++ s2ana.php5/examples/with_s2base/app/modules/s2analogin/entity/S2AnA_LoginUser.class.php
@@ -7,22 +7,58 @@ class S2AnA_LoginUser {
     const id_COLUMN = "id";
     public function setId($val){$this->id = $val;}
     public function getId(){return $this->id;}
-    
+
     protected $login;
     const login_COLUMN = "login";
     public function setLogin($val){$this->login = $val;}
     public function getLogin(){return $this->login;}
+
+    protected $email;
+    const email_COLUMN = "email";
+    public function setEmail($val){$this->email = $val;}
+    public function getEmail(){return $this->email;}
+
+    protected $firstname;
+    const firstname_COLUMN = "firstname";
+    public function setFirstName($val){$this->firstname = $val;}
+    public function getFirstName(){return $this->firstname;}
+
+    protected $lastname;
+    const lastname_COLUMN = "lastname";
+    public function setLastName($val){$this->lastname = $val;}
+    public function getLastName(){return $this->lastname;}
 
     protected $salted_password;
     const salted_password_COLUMN = "salted_password";
     public function setSalted_password($val){$this->salated_password = $val;}
     public function getSalted_password(){return $this->salted_password;}
 
+    protected $salt;
+    const salt_COLUMN = "salt";
+    public function setSalt($val){$this->salt = $val;}
+    public function getSalt(){return $this->salt;}
+
+    protected $verified;
+    const verified_COLUMN = "verified";
+    public function setVerified($val){$this->verified = $val;}
+    public function getVerified(){return $this->verified;}
+
+    protected $deleted;
+    const deleted_COLUMN = "deleted";
+    public function setDeleted($val){$this->deleted = $val;}
+    public function getDeleted(){return $this->deleted;}
+
     public function __toString() {
         $buf = array();
         $buf[] = 'id => ' . $this->getId();
         $buf[] = 'login => ' . $this->getLogin();
+        $buf[] = 'email => ' . $this->getEmail();
+        $buf[] = 'firstname => ' . $this->getFirstName();
+        $buf[] = 'lastname => ' . $this->getLastName();
         $buf[] = 'salted_password => ' . $this->getSalated_Password();
+        $buf[] = 'salt => ' . $this->getSalt();
+        $buf[] = 'verified => ' . $this->getVerified();
+        $buf[] = 'deleted => ' . $this->getDeleted();
         return '{' . implode(', ',$buf) . '}';
     }
 
@@ -32,6 +68,6 @@ class S2AnA_LoginUser {
     const prop_RELKEYS = 'this_fk:other_pk';
     public function setProp(OtherEntity $entity){ $this->prop = $entity; }
     public function getProp(){ return $this->prop; }
-    */    
+    */
 }
 ?>
