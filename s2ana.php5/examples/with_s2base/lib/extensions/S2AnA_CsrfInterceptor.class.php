b0b28b8a1e5503485a49204c1f4072d168501b67
yonekawa <yonekawa@e5899088-dff8-4fac-bb51-c4f79c91912d>
2007-07-10 04:39:14 +0000

    ''
    git-svn-id: https://www.seasar.org/svn/sandbox/s2ana.php5/trunk@30 e5899088-dff8-4fac-bb51-c4f79c91912d

---
+++ s2ana.php5/examples/with_s2base/lib/extensions/S2AnA_CsrfInterceptor.class.php
@@ -0,0 +1,99 @@
+<?php
+
+class S2AnA_CsrfInterceptor extends S2Container_AbstractInterceptor
+{
+    private $securityToken;
+
+    public function setSecurityToken(S2AnA_SecurityToken $securityToken)
+    {
+        $this->securityToken = $securityToken;
+    }
+
+    public function invoke(S2Container_MethodInvocation $invocation)
+    {
+        if (S2AnA_RequestUtility::isPost() && $this->securityToken->validateSecurityToken()) {
+            return $invocation->proceed();
+        } else {
+            throw new S2AnA_CsrfAttackException($invocation->getThis(),
+                                                $this->securityToken->getTokenName());
+        }
+    }
+}
+
+/**
+ * security_token tag
+ * <input type="hidden" name=$tokenParamName value={getSecurityToken} />
+ * token is MD5 hash for session_id
+ */
+
+class S2AnA_SecurityToken
+{
+    private $tokenName = 'security_token';
+
+    public function getSecurityToken()
+    {
+        $session_id = session_id();
+        if ( strlen( $session_id ) <= 0 ) {
+            return FALSE;
+        }
+        $token = md5($session_id);
+        return $token;
+    }
+    public function validateSecurityToken()
+    {
+        if (array_key_exists( $this->tokenName, $_POST )
+            || strlen( $_POST[$this->tokenName] ) <= 0)
+        {
+            return FALSE;
+        }
+        $postToken = $_POST[$this->tokenName];
+
+        $security_token = $this->getSecurityToken();
+        if ($security_token === FALSE) {
+            return FALSE;
+        }
+
+        // validate
+        if (strcasecmp($postToken, $security_token) !== 0) {
+            return FALSE
+        }
+        return TRUE;
+    }
+
+    public function setTokenName($tokenName)
+    {
+        $this->tokenName = $tokenName;
+    }
+    public function getTokenName()
+    {
+        return $this->tokenName;
+    }
+}
+
+class S2AnA_RequestUtility
+{
+    const GET =  'GET';
+    const POST = 'POST';
+
+    public static function isPost()
+    {
+        if (array_key_exists( 'REQUEST_METHOD', $_SERVER ) 
+            || $_SERVER['REQUEST_METHOD'] !== self::POST )
+        {
+            return FALSE;
+        }
+        return TRUE;
+    }
+
+    public static function isGet()
+    {
+        if (array_key_exists( 'REQUEST_METHOD', $_SERVER )
+            || $_SERVER['REQUEST_METHOD'] !== self::GET )
+        {
+            return FALSE;
+        }
+        return TRUE;
+    }
+}
+
+?>
